fe9dc87a40dc9b49c2be0e34dc9521d169f9994b
Julian Kippels <julkip@neunerdhausen.de>
2020-06-17 14:01:18 +0200

    differentiate between laptop and desktop blocks.h

---
+++ laptop_blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"📶", "ip addr show dev wlp61s0 | awk '/inet / {print $2}' | sed 's/\\/24//'", 60, 0},
+	{"🕸", "ip addr show dev enp0s31f6 | awk '/inet / {print $2}' | sed 's/\\/24//'", 60, 0},
+	{"💙", "LC_ALL=C top -bn 1 | awk '/^%Cpu/ {print 100 - $8\"%\"}'", 30, 0},
+	{"🧠", "free -h | awk '/^Speicher/ { print $3\"/\"$2}' | sed s/i//g",	30,		0},
+	{"💽", "df -h /home | tail -1 | awk '{print $3\"/\"$2}'",			30,		0},
+	{"⏰", "date +\"%a %d. %b %H:%M:%S\"", 1, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
